0ee32c8eb27e77383fa374a7a8f17dbc0381d849
Marcelo Hanel <hanelmarcelo@gmail.com>
2024-06-28 09:21:58 -0300

    Ajustes

--- app.py
+++ app.py
@@ -2,22 +2,25 @@ import pandas as pd
 import google.generativeai as genai
 
 pergunta = ""
-df = pd.read_excel("Vendas.xlsx")
-#df = df.drop(["Código Venda", "Quantidade", "Valor Unitário"], axis=1)
-df_text = df.to_string(index=False)
+genai.configure(api_key="AIzaSyCedQZrf-UTezu3H7vch4Ply7h_RUHJBAA")
 
-genai.configure(api_key="")
+df = pd.read_excel("kpi013d.xlsx")
+df_csv = df.to_csv("kpi013d.csv", index=False)
 
-#for m in genai.list_models():
-#  if 'generateContent' in m.supported_generation_methods:
-#    print(m.name)
+file = genai.upload_file(path="kpi013d.csv", display_name="Arquivo CSV", mime_type="text/csv")
+
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
 
 model = genai.GenerativeModel(
     model_name="gemini-1.5-pro-latest",
     generation_config=genai.GenerationConfig(
         temperature=0.0,
+        #max_output_tokens=8096,
+        response_mime_type="text/plain"
     ),
-    system_instruction="Se comporte como uma analista de dados financeiros\nSeja sempre objetivo nas respostas\nResponda sempre em portugues do Brasil",
+    system_instruction="Se comporte como uma analista de dados financeiros\nResponda sempre em portugues do Brasil\nResponda de forma objetiva",
 )
 
 chat_session = model.start_chat(
@@ -25,20 +28,19 @@ chat_session = model.start_chat(
         {
             "role": "user",
             "parts": [
-                df_text,
+                file,
             ],
         },        
     ]
 )
-response = chat_session.send_message("Descreva este relatório em portugues do Brasil")
+
+chat_session.send_message
+
+response = chat_session.send_message("Descreva este relatório em portugues do Brasil e me liste possíveis análises que posso fazer com ele")
 print(response.text)
 
-
-#print(df_text)
-#print()
-#print()
-
 while pergunta != "sair": 
+
     pergunta = input("Digite sua pergunta: ")
     if pergunta == "sair":
         break
